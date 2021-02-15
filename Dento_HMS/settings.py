301cf575eeaf4e3fbb8ae3bcfff5467ff5192db0
kavyaraviprakash <70050485+kavyaraviprakash@users.noreply.github.com>
2021-02-15 03:04:08 -0600

    Update settings.py

--- Dento_HMS/settings.py
+++ Dento_HMS/settings.py
@@ -116,7 +116,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-#AUTH_USER_MODEL = 'hospital.CustomUser'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
