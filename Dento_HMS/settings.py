28ee6c699359340f0a3a6179f375e907c574bdec
kayaraviprakash <kraviprakash@unomaha.edu>
2021-04-08 08:36:44 -0500

    recent changes on static files

--- Dento_HMS/settings.py
+++ Dento_HMS/settings.py
@@ -122,11 +122,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL ='/media/'
+STATIC_URL = '/static/'
+
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
