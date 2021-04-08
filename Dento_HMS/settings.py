f069c4cc0afddad50e90e0cc7a2f18686c56e73f
kayaraviprakash <kraviprakash@unomaha.edu>
2021-04-08 11:56:17 -0500

    removing whitenosie files

--- Dento_HMS/settings.py
+++ Dento_HMS/settings.py
@@ -50,7 +50,6 @@ EMAIL_USE_TLS = True
 
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,6 +126,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
